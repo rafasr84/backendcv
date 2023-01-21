649764d7b537f256012664958785cdfe05f596d3
Rafael <rafaelsr84@gmail.com>
2023-01-20 23:18:50 -0300

    actualización de URL Frontend en Firebase definitiva

--- src/main/java/com/backendcv/backendcv/Controller/CExperiencia.java
+++ src/main/java/com/backendcv/backendcv/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "https://frontendcv-rasr.web.app/")
+@CrossOrigin(origins = "https://frontendrafa.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
